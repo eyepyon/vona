b7e324f9f2944c330913e3618b7cd79e2f242207
eye <eye@frog.company>
2024-10-12 09:04:23 +0900

    init vonage

---
+++ laravel/app/Console/SendSms.php
@@ -0,0 +1,16 @@
+<?php
+use \App\Notifications\VonageNotification;
+class SendSms extends Command
+{
+    protected $signature = 'send:sms';
+
+    public function handle()
+    {
+        $message = $this->ask('What message do you want to send?');
+
+        $to = '+818048563939';
+        Notification::route('vonage', $to)->notify(new VonageNotification($message));
+    }
+}
+
+
